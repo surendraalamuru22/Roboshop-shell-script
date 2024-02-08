81653767899321308c9565d29a5df746f26d25f6
surendra reddy <surendrareddylamuru01@gmail.com>
2024-02-08 13:22:00 +0530

    sending output to a file

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,22 @@
 echo Installing Nginx
-yum install nginx -y
+yum install nginx -y &>>/tmp/frontend
 systemctl enable nginx
 systemctl start nginx
 
 echo Downloading Nginx Web Content
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend
+
 cd /usr/share/nginx/html
 
+
 echo Removing Old Web Content
-rm -rf *
+rm -rf * &>>/tmp/frontend
 
 echo Extracting Web Content
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>/tmp/frontend
 
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 
 echo Starting Nginx Service
-systemctl restart nginx
+systemctl restart nginx &>>/tmp/frontend
