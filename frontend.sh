ff418bb7dd3d4c74952d9502115581e75885c1ef
surendra reddy <surendrareddylamuru01@gmail.com>
2024-02-09 15:41:34 +0530

    functions.sh

--- frontend.sh
+++ frontend.sh
@@ -1,29 +1,32 @@
 LOG_FILE=/tmp/frontend
+
+source common.sh
+
 echo Installing Nginx
 yum install nginx -y &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 systemctl enable nginx
 systemctl start nginx
 
 echo Downloading Nginx Web Content
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 cd /usr/share/nginx/html
 
 
 echo Removing Old Web Content
 rm -rf * &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 echo Extracting Web Content
 unzip /tmp/frontend.zip &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 
 echo Starting Nginx Service
 systemctl restart nginx &>>LOG_FILE
-echo status = $?
+StatusCheck $?
