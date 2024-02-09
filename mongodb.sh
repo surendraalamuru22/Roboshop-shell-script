2948e94cd3e6561925084551d4d2892dcbf7a977
surendra reddy <surendrareddylamuru01@gmail.com>
2024-02-09 15:38:56 +0530

    functions.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,37 +1,40 @@
 LOG_FILE=/tmp/mongodb
+
+source common.sh
+
 echo "Setting MongoDB Repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 echo "Installing MongoDB Server"
 yum install -y mongodb-org &>>LOG_FILE
 systemctl start mongod &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 echo "Update MongoDB Listen Address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-echo status = $?
+StatusCheck $?
 
 echo "Starting MongoDB Service"
 systemctl enable mongod &>>LOG_FILE
 systemctl restart mongod &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 echo "Downloading MongoDb Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 cd /tmp
 
 echo "Extract Schema File"
 unzip -o mongodb.zip &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 cd mongodb-main
 
 echo "Load Schema"
 mongo < catalogue.js &>>LOG_FILE
 mongo < users.js &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 
