7377eaa52915a198d4312acb5d9d632891ceea22
surendra reddy <surendrareddylamuru01@gmail.com>
2024-02-09 18:40:46 +0530

    mysql.sh

---
+++ mysql.sh
@@ -0,0 +1,26 @@
+LOG_FILE=/tmp/mysql
+source common.sh
+
+echo "Setting Up MySQL Repo"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG_FILE
+StatusCheck $?
+
+echo "Disable MySQL Default Module to Enable 5.7 MySQL"
+dnf module disable mysql -y &>>$LOG_FILE
+StatusCheck $?
+
+echo "Install MySQL"
+yum install mysql-community-server -y &>>$LOG_FILE
+StatusCheck $?
+
+
+echo "Start MySQL Service"
+systemctl enable mysqld &>>$LOG_FILE
+systemctl restart mysqld &>>$LOG_FILE
+StatusCheck $?
+
+
+DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+
+echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
+FLUSH PRIVILEGES;" >/tmp/root-pass.sql
