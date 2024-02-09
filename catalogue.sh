6c06328c9cddc0b09982b7e759b128dbda3e314c
surendra reddy <surendrareddylamuru01@gmail.com>
2024-02-09 15:35:49 +0530

    functions.sh

--- catalogue.sh
+++ catalogue.sh
@@ -1,19 +1,6 @@
 LOG_FILE=/tmp/catalogue
 
-ID=$(id -u)
-if [ $ID -ne 0 ]; then
-  echo you should run this script as root user or with sudo previlages
-  exit 1
-fi
-
-StatusCheck() {
-  if [ $1 -eq 0 ]; then
-    echo -e status = "\e[32msuccess\e[0m"
-  else
-    echo -e status = "\e[31mfailure\e[0m"
-    exit 1
-  fi
-}
+source common.sh
 
 
 echo "Setting nodejs Repo"
