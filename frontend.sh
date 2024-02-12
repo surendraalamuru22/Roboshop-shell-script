5acb287ac5fb2b2c766e0ba45fb964c4006d1758
surendra reddy <surendrareddylamuru01@gmail.com>
2024-02-12 10:45:27 +0530

    frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -27,6 +27,13 @@ StatusCheck $?
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 
+echo "Update RoboShop Config File"
+for component in catalogue user cart payment shipping ; do
+  sed -i -e "/$component/ s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+done
+#sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+StatusCheck $?
+
 echo Starting Nginx Service
 systemctl restart nginx &>>LOG_FILE
 StatusCheck $?
